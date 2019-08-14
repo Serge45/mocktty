8f791dc922094e922dc2e86b8d05ffc82e4414d7
Serge45 <Serge45497@gmail.com>
2019-08-15 00:20:45 +0800

    Initial commit.

---
+++ mocktty.py
@@ -0,0 +1,37 @@
+import os
+import pty
+import serial
+import codecs
+import threading as mt
+
+def main():
+    master, slave = pty.openpty()
+    s_name = os.ttyname(slave)
+
+    print('mock serial port: {}'.format(s_name))
+
+    ser = serial.Serial(s_name)
+
+    def fix_escape(s):
+        return codecs.decode(s, 'unicode_escape')
+
+    def to_bytes(s):
+        return s.encode('ISO-8859-1')
+
+    def reader_func():
+        while True:
+            r = os.read(master, 256)
+
+            if (len(r)):
+                print('Bytes received: {}'.format(r))
+
+    reader_thread = mt.Thread(target=reader_func)
+    reader_thread.start()
+
+    while True:
+        msg = input()
+        bytes_to_send = to_bytes(fix_escape(msg))
+        os.write(master, bytes_to_send)
+
+if __name__ == '__main__':
+    main()
---
+++ readme.md
@@ -0,0 +1,12 @@
+## Introduction
+Fake tty device by using pyserial
+
+## Usage
+ - `pip install -r requirements.txt`
+ - python3 mocktty.py
+
+ console will print the name of created tty device, and then you can 
+  - send bytes to client via stdin, e.g. `'\xF0\x00\xF7'`
+  - `$ hexdump < <path_of_mock_tty_device>` to get the buffered data in client
+  - `$ echo -n -e '<bytes> > <path_of_mock_tty_device>` to send bytes to master
+  - data sent to master will output via stdout
